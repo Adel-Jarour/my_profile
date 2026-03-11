e7864f502639a0d2a9897ce36b8289f08bde68cb
adel <adel@gmail.com>
2026-03-11 16:36:00 +0200

    update navbar

--- lib/view/screens/home/widget/header_bar_widget.dart
+++ lib/view/screens/home/widget/header_bar_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:my_profile/config/translation/strings_enum.dart';
 import 'package:my_profile/const/color_const.dart';
@@ -17,64 +19,77 @@ class HeaderBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 56,
-      child: Row(
-        children: [
-          Container(
-            width: 32,
-            height: 32,
-            decoration: BoxDecoration(
-              color: ColorConst.whiteColor,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: ColorConst.thirdColor.withValues(alpha: 0.15),
-              ),
-            ),
-            child: const Icon(
-              Icons.code_rounded,
-              color: ColorConst.thirdColor,
-              size: 18,
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(25)),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+        child: Container(
+          height: 56,
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            color: Colors.grey.withValues(alpha: 0.1),
+            borderRadius: BorderRadius.all(
+              Radius.circular(30),
             ),
           ),
-          const SizedBox(width: 12),
-          CustomText(
-            Strings.logoTitle,
-            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: ColorConst.thirdColor,
-                  fontWeight: FontWeight.w600,
-                ),
-          ),
-          const Spacer(),
-          if (isDesktop)
-            Row(
-              children: controller.sections
-                  .map(
-                    (e) => Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 6),
-                      child: NavItemWidget(
-                        controller: controller,
-                        index: e.id,
-                        label: e.title,
-                        isDesktop: isDesktop,
-                      ),
-                    ),
-                  )
-                  .toList(),
-            )
-          else
-            Builder(
-              builder: (context) {
-                return IconButton(
-                  onPressed: Scaffold.of(context).openDrawer,
-                  icon: const Icon(
-                    Icons.menu_rounded,
-                    color: ColorConst.thirdColor,
+          child: Row(
+            children: [
+              Container(
+                width: 32,
+                height: 32,
+                decoration: BoxDecoration(
+                  color: ColorConst.whiteColor,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: ColorConst.thirdColor.withValues(alpha: 0.15),
                   ),
-                );
-              },
-            ),
-        ],
+                ),
+                child: const Icon(
+                  Icons.code_rounded,
+                  color: ColorConst.thirdColor,
+                  size: 18,
+                ),
+              ),
+              const SizedBox(width: 12),
+              CustomText(
+                Strings.logoTitle,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: ColorConst.thirdColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+              const Spacer(),
+              if (isDesktop)
+                Row(
+                  children: controller.sections
+                      .map(
+                        (e) => Padding(
+                          padding: const EdgeInsetsDirectional.only(start: 8),
+                          child: NavItemWidget(
+                            controller: controller,
+                            index: e.id,
+                            label: e.title,
+                            isDesktop: isDesktop,
+                          ),
+                        ),
+                      )
+                      .toList(),
+                )
+              else
+                Builder(
+                  builder: (context) {
+                    return IconButton(
+                      onPressed: Scaffold.of(context).openDrawer,
+                      icon: const Icon(
+                        Icons.menu_rounded,
+                        color: ColorConst.thirdColor,
+                      ),
+                    );
+                  },
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
